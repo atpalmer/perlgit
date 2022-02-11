934a766d42d5972ebe1434c412a7e92be17f3bff
Andrew Palmer <atp@sdf.org>
2022-02-11 15:38:50 -0600

    Extract modules

---
+++ lib/GitFile.pm
@@ -0,0 +1,39 @@
+use strict;
+use warnings FATAL => qw/all/;
+use autodie;
+
+
+package GitFile;
+
+use Compress::Zlib;
+
+sub read_zfile {
+    my $path = shift;
+    my $data = read_binary_file($path);
+    return z_inflate($data);
+}
+
+sub z_inflate {
+    my $data = shift;
+    my ($i, $status, $result);
+
+    ($i, $status) = inflateInit();
+    die if $status != Z_OK;
+
+    ($result, $status) = $i->inflate($data);
+    die $status if $status != Z_STREAM_END;
+
+    return $result;
+}
+
+sub read_binary_file {
+    my $path = shift;
+    open my $f, '<', $path;
+    binmode $f;
+    local($/);
+    my ($data) = <$f>;
+    close $f;
+    return $data;
+}
+
+1;
