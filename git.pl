c3ecb01dcbed6e0895a8f7b36e4acbc9831fe3f8
Andrew Palmer <atp@sdf.org>
2022-02-22 13:39:55 -0600

    Read path from command line switch

--- git.pl
+++ git.pl
@@ -6,9 +6,37 @@ use autodie;
 use lib 'lib';
 use GitObject;
 
-my $path = shift or die;
+my $opts = Opts->parse('path=s');
 
-my $object = GitObject->from_path($path);
+my $object = GitObject->from_path($opts->{path});
 
 $object->say;
 
+
+package Opts {
+    use Getopt::Long;
+
+    sub parse {
+        my $class = shift;
+        my $config = shift;
+        my %opts = ();
+        GetOptions(\%opts, $config) or die 'Bad Arguments';
+
+        my %new;
+        tie %new, $class, \%opts;
+        return \%new;
+    }
+
+    sub TIEHASH {
+        my $class = shift;
+        my $new = shift;
+        return bless $new, $class;
+    }
+
+    sub FETCH {
+        my $self = shift;
+        my $key = shift;
+        die qq<No option "$key"> if not defined $self->{$key};
+        return $self->{$key};
+    }
+}
